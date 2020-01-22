a30643099af166f7539382f2e550f962a94aa81a
alfianjau <alfianjauhari@gmail.com>
2020-01-22 11:37:58 +0700

    add gltf file

--- components/3dPlane/Plane.vue
+++ components/3dPlane/Plane.vue
@@ -8,6 +8,7 @@
 /* eslint-disable */
 import * as THREE from 'three'
 import  GLTFLoader  from 'three-gltf-loader';
+import OrbitControls  from "three-orbitcontrols"
 export default {
   name: 'Plane',
   props: {
@@ -30,41 +31,53 @@ export default {
     canvas.height = window.innerHeight;
     canvas.width = window.innerWidth;
     const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xF0F8FF);
-    const camera = new THREE.PerspectiveCamera(40,canvas.width/canvas.height,1,5000);
-    camera.rotation.y = 45/180*Math.PI;
+    scene.background = new THREE.Color(0xffffff);
+    const camera = new THREE.PerspectiveCamera(40, canvas.width / canvas.height, 1, 5000);
+    camera.rotation.y = 45 / 180 * Math.PI;
     camera.position.x = 800;
     camera.position.y = 100;
     camera.position.z = 1000;
-    const renderer = new THREE.WebGLRenderer({antialias:true,canvas:canvas});
-    renderer.setClearColor(new THREE.Color(0xEEEEEE, 1.0));
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      canvas: canvas
+    });
+    // renderer.setClearColor(0xd8e7ff, 0);
     // renderer.setSize(canvas.width/canvas.height);
     // document.body.appendChild(renderer.domElement);
-    const hlight = new THREE.AmbientLight (0x404040,100);
+    const hlight = new THREE.AmbientLight(0x404040, 100);
     scene.add(hlight);
-    const directionalLight = new THREE.DirectionalLight(0xffffff,100);
-    directionalLight.position.set(0,1,0);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 100);
+    directionalLight.position.set(0, 1, 0);
     directionalLight.castShadow = true;
     scene.add(directionalLight);
-    const light = new THREE.PointLight(0xc4c4c4,10);
-    light.position.set(0,300,500);
+    const light = new THREE.PointLight(0xc4c4c4, 10);
+    light.position.set(0, 300, 500);
     scene.add(light);
-    const light2 = new THREE.PointLight(0xc4c4c4,10);
-    light2.position.set(500,100,0);
+    const light2 = new THREE.PointLight(0xc4c4c4, 10);
+    light2.position.set(500, 100, 0);
     scene.add(light2);
-    const light3 = new THREE.PointLight(0xc4c4c4,10);
-    light3.position.set(0,100,-500);
+    const light3 = new THREE.PointLight(0xc4c4c4, 10);
+    light3.position.set(0, 100, -500);
     scene.add(light3);
-    const light4 = new THREE.PointLight(0xc4c4c4,10);
-    light4.position.set(-500,300,500);
+    const light4 = new THREE.PointLight(0xc4c4c4, 10);
+    light4.position.set(-500, 300, 500);
     scene.add(light4);
     const loader = new GLTFLoader();
-    loader.load('https://threejsfundamentals.org/threejs/resources/models/cartoon_lowpoly_small_city_free_pack/scene.gltf', function(gltf){
-    let car = gltf.scene.children[0];
-    car.scale.set(0.5,0.5,0.5);
-    scene.add(gltf.scene);
-    // animate();
-});
+    loader.load('/scene.gltf', function (gltf) {
+      let car = gltf.scene.children[0];
+      car.scale.set(0.3, 0.3, 0.3);
+      scene.add(gltf.scene);
+      animate();
+
+      // const controls = new OrbitControls(camera, canvas);
+      // controls.addEventListener('change', animate);
+
+      function animate() {
+        renderer.render(scene, camera);
+        // requestAnimationFrame(animate);
+      }
+
+    });
   }
 }
 </script>
